30834041a1219723858d348254cf4128277809a0
sugarkwork <sugarkwork@sugar-knight.com>
2025-04-12 00:04:46 +0900

    update

--- trtagger.py
+++ trtagger.py
@@ -6,15 +6,26 @@ from PIL import Image
 import csv
 import os
 import subprocess
+import requests
+import os
 
 TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
 
 class TensorRTTagger:
-    model_dir = "models"
+    models_dir = "models"
     
-    def __init__(self, engine_path, tag_csv_path):
+    def __init__(self, model="wd-eva02-large-tagger-v3"):
         # エンジン読み込み
-        self.engine = self.load_engine(os.path.join(self.model_dir, engine_path))
+        self.download_model(model)
+        engine_name = f"{model}.trt"
+        tag_csv_name = f"{model}.csv"
+        engine_path = os.path.join(self.models_dir, engine_name)
+        tag_csv_path = os.path.join(self.models_dir, tag_csv_name)
+
+        if not os.path.exists(engine_path):
+            TensorRTTagger.convert(f"{model}.onnx", engine_name)
+
+        self.engine = self.load_engine(engine_path)
         self.context = self.engine.create_execution_context()
         self.input_name = self.engine.get_tensor_name(0)
         self.output_name = self.engine.get_tensor_name(1)
@@ -23,7 +34,7 @@ class TensorRTTagger:
         self.output_shape = self.engine.get_tensor_shape(self.output_name)
 
         # タグリスト読み込み
-        self.tags, self.general_index, self.character_index = self.load_tags(os.path.join(self.model_dir, tag_csv_path))
+        self.tags, self.general_index, self.character_index = self.load_tags(tag_csv_path)
 
     def load_engine(self, engine_path):
         runtime = trt.Runtime(TRT_LOGGER)
@@ -63,6 +74,11 @@ class TensorRTTagger:
             images = self.load_images([images])
         if isinstance(images[0], str):
             images = self.load_images(images)
+        if isinstance(images, Image.Image):
+            images = [("image", images)]
+        if isinstance(images[0], Image.Image):
+            images = [(f"image{i}", img) for i,img in enumerate(images)]
+
         filenames_all = [filename for filename, _ in images]
         preprocessed_all = [self.preprocess(img) for _, img in images]
 
@@ -102,15 +118,60 @@ class TensorRTTagger:
     
     @staticmethod
     def convert(onnx_path, trt_path):
-        if os.path.exists(os.path.join(TensorRTTagger.model_dir, trt_path)):
+        if os.path.exists(os.path.join(TensorRTTagger.models_dir, trt_path)):
             return
         # convert
-        cmd = f"trtexec --onnx={os.path.join(TensorRTTagger.model_dir, onnx_path)} --saveEngine={os.path.join(TensorRTTagger.model_dir, trt_path)} --minShapes=input:1x448x448x3 --optShapes=input:4x448x448x3 --maxShapes=input:8x448x448x3 --verbose"
+        cmd = f"trtexec --onnx={os.path.join(TensorRTTagger.models_dir, onnx_path)} --saveEngine={os.path.join(TensorRTTagger.models_dir, trt_path)} --minShapes=input:1x448x448x3 --optShapes=input:4x448x448x3 --maxShapes=input:8x448x448x3 --verbose"
         subprocess.run(cmd, shell=True)
     
     @staticmethod
     def load_images(image_paths: list[str]) -> list[tuple[str, Image.Image]]:
         return [(path, Image.open(path)) for path in image_paths]
+
+    @staticmethod
+    def get_ext_dir(subpath=None, mkdir=False):
+        dir = os.path.dirname(__file__) if '__file__' in locals() else ''
+        if subpath is not None:
+            dir = os.path.join(dir, subpath)
+        dir = os.path.abspath(dir)
+        if mkdir and not os.path.exists(dir):
+            os.makedirs(dir)
+        return dir
+
+    def get_installed_models(self):
+        return filter(lambda x: x.endswith(".onnx"), os.listdir(self.models_dir))
+
+    def download_model(self, model):
+        installed = list(self.get_installed_models())
+        if any(model + ".onnx" in s for s in installed):
+            return
+
+        url = f"https://huggingface.co/SmilingWolf/{model}/resolve/main/"
+        print(f"Downloading {model} ( {url} )")
+        
+        self.download_to_file(
+            f"{url}model.onnx", os.path.join(self.models_dir, f"{model}.onnx"))
+        self.download_to_file(
+            f"{url}selected_tags.csv", os.path.join(self.models_dir, f"{model}.csv"))
+        print(f"Downloaded {model}")
+        return
+
+    def download_to_file(self, url, destination, is_ext_subpath=True):
+        if is_ext_subpath:
+            destination = self.get_ext_dir(destination)
+        
+        try:
+            response = requests.get(url, stream=True)
+            response.raise_for_status()  # エラーがあれば例外を投げる
+            
+            with open(destination, mode='wb') as f:
+                for chunk in response.iter_content(chunk_size=2048):
+                    if chunk:  # 空のチャンクをスキップ
+                        f.write(chunk)
+        
+        except requests.RequestException as e:
+            print(f"Error downloading {url}: {e}")
+            raise
 
 
 def main():
@@ -130,25 +191,19 @@ def main():
     import json
 
     image_filenames = glob.glob("test*.png") + glob.glob("test*.jpg")
-    images = [(filename, Image.open(filename)) for filename in image_filenames]
 
-    TensorRTTagger.convert("wd-eva02-large-tagger-v3.onnx", "wd-eva02-large-tagger-v3.trt")
-
-    tagger = TensorRTTagger(
-        engine_path="wd-eva02-large-tagger-v3.trt",
-        tag_csv_path="wd-eva02-large-tagger-v3.csv"
-    )
+    tagger = TensorRTTagger(model="wd-eva02-large-tagger-v3")
 
     print()
     start_time = time.time()
-    for image in images:
-        results = tagger.infer_batch(image[0])
+    for image in image_filenames:
+        results = tagger.infer_batch(image)
         print(json.dumps(results, indent=4))
     print(f"elapsed time: {time.time() - start_time}")
 
     print()
     start_time = time.time()
-    results = tagger.infer_batch(images)
+    results = tagger.infer_batch(image_filenames)
     print(json.dumps(results, indent=4))
     print(f"elapsed time: {time.time() - start_time}")
 
